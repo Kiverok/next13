49e246c17cf1ae0d58467be7d7a3a4a437a0c3d9
Kiverok <kiver566@ukr.net>
2023-06-05 19:28:05 +0300

    activeLink

---
+++ components/Navigation.tsx
@@ -0,0 +1,35 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+type Navlink = {
+    label: string;
+    href: string;
+};
+type Props = {
+    navLinks: Navlink[];
+};
+
+const Navigation = ({ navLinks }: Props) => {
+    const pathname = usePathname ();
+
+    return <>{
+        navLinks.map((link) => {
+const isActive = pathname === link.href;
+
+return(
+    <Link
+    key={link.label}
+    href={link.href}
+    className={isActive ? "active" : ""}
+    >
+    {link.label}
+    </Link>
+)
+        }
+        )}
+    </>;
+};
+
+export { Navigation }
+
